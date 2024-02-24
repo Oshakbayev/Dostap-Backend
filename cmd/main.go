1345d46304d6a71a79065a7d31bca22e746c2e8f
Rakatan2024 <rakatan228322@gmail.com>
2024-02-24 23:15:09 +0600

    initserver

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	port := "80"
+	router := http.NewServeMux()
+	router.HandleFunc("/", MainPage)
+
+	fmt.Println("listening on: http://localhost:" + port + "/")
+	err := http.ListenAndServe(":"+port, router)
+	if err != nil {
+		fmt.Println("Error: several connecrtions")
+		return
+	}
+}
+
+func MainPage(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello world!"))
 }
--- pkg/custom_router.go
+++ pkg/custom_router.go
@@ -1,33 +1,33 @@
-package pkg
-
-import "net/http"
-
-type Handler func(w http.ResponseWriter, r *http.Request)
-
-type route struct {
-	Path    string
-	Method  string
-	Handler Handler
-}
-
-type Router struct {
-	routes []route
-}
-
-func (r *Router) AddRoute(path string, method string, handler Handler) {
-	r.routes = append(r.routes, route{Path: path, Method: method, Handler: handler})
-}
-func (r *Router) Get(path string, handler Handler) {
-	r.AddRoute(path, http.MethodGet, handler)
-}
-
-func (r *Router) Post(path string, handler Handler) {
-	r.AddRoute(path, http.MethodPost, handler)
-}
-
-func (r *Router) getHandler(path, method string) {
-	regexp := regexp.MustComipile
-	for _, route := range r.routes {
-
-	}
-}
+//package pkg
+//
+//import "net/http"
+//
+//type Handler func(w http.ResponseWriter, r *http.Request)
+//
+//type route struct {
+//	Path    string
+//	Method  string
+//	Handler Handler
+//}
+//
+//type Router struct {
+//	routes []route
+//}
+//
+//func (r *Router) AddRoute(path string, method string, handler Handler) {
+//	r.routes = append(r.routes, route{Path: path, Method: method, Handler: handler})
+//}
+//func (r *Router) Get(path string, handler Handler) {
+//	r.AddRoute(path, http.MethodGet, handler)
+//}
+//
+//func (r *Router) Post(path string, handler Handler) {
+//	r.AddRoute(path, http.MethodPost, handler)
+//}
+//
+//func (r *Router) getHandler(path, method string) {
+//	regexp := regexp.MustComipile
+//	for _, route := range r.routes {
+//
+//	}
+//}
