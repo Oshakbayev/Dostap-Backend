72a1da4c5740b4daf3d2e50d4f40ef5ce9c91ccc
Makhayev <y.makhayev@alabs.team>
2024-02-25 18:33:29 +0600

    feat: add connection to db

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,91 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	_ "github.com/lib/pq"
 )
 
-func main() {
-	port := "80"
-	router := http.NewServeMux()
-	router.HandleFunc("/", MainPage)
+var db *sql.DB
 
-	fmt.Println("listening on: http://localhost:" + port + "/")
-	err := http.ListenAndServe(":"+port, router)
+type User struct {
+    ID   int
+    Name string
+    Role string
+}
+
+func initDB() {
+	var err error
+	connectionString := "postgres://owwkdlwj:UqnYqPkMlDPUHBntLlFYpIeLjaXZkCxR@abul.db.elephantsql.com/owwkdlwj"
+	db, err = sql.Open("postgres", connectionString)
+
 	if err != nil {
-		fmt.Println("Error: several connecrtions")
-		return
+		fmt.Println("kek")
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("kek2")
+		log.Fatal(err)
 	}
 }
 
-func MainPage(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello world!"))
+func main() {
+	initDB()
+	defer db.Close()
+
+	port := "80"
+	router := http.NewServeMux()
+	router.HandleFunc("/", MainPage)
+	fmt.Println("kek3")
+	fmt.Println("Listening on: http://localhost:" + port + "/")
+	if err := http.ListenAndServe(":"+port, router); err != nil {
+		log.Fatal("Error starting server: ", err)
+	}
 }
+
+func QueryUsers(db *sql.DB) ([]User, error) {
+    var users []User
+
+    rows, err := db.Query("SELECT id, name, role FROM users")
+    if err != nil {
+        return nil, err // return an error immediately
+    }
+    defer rows.Close()
+
+    for rows.Next() {
+        var u User
+        err := rows.Scan(&u.ID, &u.Name, &u.Role)
+        if err != nil {
+            return nil, err // return an error immediately
+        }
+        users = append(users, u)
+    }
+
+    if err = rows.Err(); err != nil {
+        return nil, err
+    }
+
+    return users, nil // return the slice of users
+}
+
+
+func MainPage(w http.ResponseWriter, r *http.Request) {
+    users, err := QueryUsers(db)
+    if err != nil {
+        http.Error(w, "Internal Server Error", 500)
+        log.Println("QueryUsers error:", err)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    if err := json.NewEncoder(w).Encode(users); err != nil {
+        http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
+        log.Println("JSON encoding error:", err)
+    }
+}
+
