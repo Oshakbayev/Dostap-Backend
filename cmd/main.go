7e02b92b9c0fdaad8b504c25890dba06422a31b6
Rakatan2024 <rakatan228322@gmail.com>
2024-02-25 21:06:03 +0600

    webhook test 2.3.3

--- cmd/main.go
+++ cmd/main.go
@@ -37,7 +37,7 @@ func initDB() {
 func main() {
 	initDB()
 	defer db.Close()
-	fmt.Println("Ansaer Loh and Pidaraz and Mal and Crenge and Kocnita and HUi")
+	fmt.Println("Ansaer Loh and Pidaraz and Mal and Crenge and Kocnita and HUi and Dalbayeb")
 	port := "80"
 	router := http.NewServeMux()
 	router.HandleFunc("/", MainPage)
