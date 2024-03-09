178495f8a14d57e6563bbf2dc681b1c5e35ee4b5
Rakatan2024 <rakatan228322@gmail.com>
2024-03-09 23:07:16 +0500

    test of ci/cd 1.0

--- cmd/main.go
+++ cmd/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	fmt.Println("TEST of deploy.sh")
+	fmt.Println("Tpotentially final test of ci/cd ")
 	config := configPkg.CreateConfig()
 	if err := configPkg.ReadConfig("cmd/config/devConfig.json", config); err != nil {
 		log.Fatal(err)
