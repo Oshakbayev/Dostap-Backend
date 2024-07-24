3b3d76951b77390172ab7860036025da241162f7
plony <esiredrise@gmail.com>
2024-07-24 15:07:17 +0300

    added essential dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,19 +2,44 @@ plugins {
     alias(libs.plugins.jvm)
     application
     alias(libs.plugins.ktor)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 repositories {
     mavenCentral()
 }
 
+val exposedVersion = libs.versions.exposed.get()
+val koinVersion = libs.versions.koin.asProvider().get()
+val koinKspVersion = libs.versions.koin.ksp.get()
 dependencies {
     implementation("io.ktor:ktor-server-core")
     implementation("io.ktor:ktor-server-netty")
+    implementation("io.ktor:ktor-server-content-negotiation")
+    implementation("io.ktor:ktor-serialization-kotlinx-json")
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-crypt:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposedVersion")
+
+
+    implementation("io.insert-koin:koin-core:$koinVersion")
+    implementation("io.insert-koin:koin-ktor:$koinVersion")
+    implementation("io.insert-koin:koin-logger-slf4j:$koinVersion")
+
+    implementation("io.insert-koin:koin-annotations:$koinKspVersion")
+    ksp("io.insert-koin:koin-ksp-compiler:$koinKspVersion")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation(libs.junit.jupiter.engine)
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+sourceSets.main {
+    java.srcDirs("build/generated/ksp/main/kotlin")
 }
 
 java {
--- app/src/main/kotlin/kz/dostap/app/Main.kt
+++ app/src/main/kotlin/kz/dostap/app/Main.kt
@@ -1,10 +1,19 @@
 package kz.dostap.app
 
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import kotlinx.serialization.json.Json
 
 fun main(args: Array<String>) = io.ktor.server.netty.EngineMain.main(args)
 
 
+@Suppress("unused")
 fun Application.module() {
-
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+            ignoreUnknownKeys = true
+        })
+    }
 }
